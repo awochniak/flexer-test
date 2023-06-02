b1c47a487c01762c1cf6107002224c9b6f67e81a
awochniak <45997707+awochniak@users.noreply.github.com>
2023-06-02 11:00:13 +0200

    Update ecommercy_3.js

--- ecommercy_3.js
+++ ecommercy_3.js
@@ -1,3 +1,8 @@
-(function() {
-  console.log("test")
-})
+export const name = "square";
+
+export function draw(ctx, length, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, length, length);
+
+  return { length, x, y, color };
+}
